47221a7ad75671d1b32dd857e388a2234d266711
MstrPikachu <31784486+MstrPikachu@users.noreply.github.com>
2019-05-13 22:01:07 -0400

    Centered title
    Used SwingConstants.CENTER in the title's JLabel constructor

--- Frame.java
+++ Frame.java
@@ -3,22 +3,17 @@ import javax.swing.*;
 
 public class Frame extends JFrame {
 	private JPanel intro = new JPanel();
-	private Dimension preferredSize = new Dimension(640, 480);
+	private static final Dimension preferredSize = new Dimension(640, 480);
 	public Frame(){
 		//set up JFrame
 		super("Organization Nation");
 
 		//set up intro screen
-		System.out.println("a");
-		JLabel title = new JLabel("<html><center>Organization Nation</html>");
-		System.out.println("b");
+		JLabel title = new JLabel("Organization Nation", SwingConstants.CENTER);
 		intro.setLayout(new BorderLayout());
 		intro.add(title, BorderLayout.CENTER);
 		intro.setBackground(new Color(255, 0, 0));
 		
-
-
-
 
 		super.setVisible(true);
 		super.pack();
